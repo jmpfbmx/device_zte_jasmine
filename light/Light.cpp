ac6040a1ca69a2ddc92e149907d9f06cac5df1aa
Ethan Chen <intervigil@gmail.com>
2017-09-17 02:27:36 -0700

    hiae: Convert lights HAL to native binderized HAL
    Change-Id: I43673573af05d1118cbcd3d0146749f143745a84

---
+++ light/Light.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "light"
+
+#include "Light.h"
+
+#include <log/log.h>
+
+namespace {
+using android::hardware::light::V2_0::LightState;
+
+static const char* const lcd_backlight_filename =
+    "/sys/class/leds/lcd-backlight/brightness";
+
+static void
+write_file(const char *path, const char *str)
+{
+    int fd;
+    ssize_t written = -1;
+
+    ALOGI("%s: path=%s str=%s\n", __func__, path, str);
+    fd = open(path, O_RDWR);
+    if (fd < 0) {
+        ALOGE("%s: Failed to open %s: %s\n",
+                __func__, path, strerror(errno));
+        return;
+    }
+    written = write(fd, str, strlen(str));
+    if (written < 0) {
+        ALOGE("%s: Failed to write to %s: %s\n",
+                __func__, path, strerror(errno));
+    }
+    close(fd);
+}
+
+static uint32_t rgbToBrightness(const LightState& state) {
+    uint32_t color = state.color & 0x00ffffff;
+    return ((77 * ((color >> 16) & 0xff)) + (150 * ((color >> 8) & 0xff)) +
+            (29 * (color & 0xff))) >> 8;
+}
+
+} // anonymous namespace
+
+namespace android {
+namespace hardware {
+namespace light {
+namespace V2_0 {
+namespace implementation {
+
+static constexpr uint32_t MODE_ON = 0x1;
+static constexpr uint32_t MODE_BLINK = 0x2;
+
+static constexpr uint32_t MODE_SHIFT = 24;
+static constexpr uint32_t MODE_MASK = 0x0f000000;
+
+Light::Light()
+{
+    auto backlightFn(std::bind(&Light::setBacklight, this, std::placeholders::_1));
+    mLights.emplace(std::make_pair(Type::BACKLIGHT, backlightFn));
+}
+
+// Methods from ::android::hardware::light::V2_0::ILight follow.
+Return<Status> Light::setLight(Type type, const LightState& state) {
+    if (mLights.find(type) != mLights.end()) {
+        mLights.at(type)(state);
+        return Status::SUCCESS;
+    }
+    return Status::LIGHT_NOT_SUPPORTED;
+}
+
+Return<void> Light::getSupportedTypes(getSupportedTypes_cb _hidl_cb) {
+    Type *types = new Type[mLights.size()];
+    int idx = 0;
+
+    for (auto const &kv : mLights) {
+        Type t = kv.first;
+        types[idx++] = t;
+    }
+
+    {
+        hidl_vec<Type> hidl_types{};
+        hidl_types.setToExternal(types, mLights.size());
+
+        _hidl_cb(hidl_types);
+    }
+
+    delete[] types;
+
+    return Void();
+}
+
+void Light::setBacklight(const LightState& state) {
+    std::lock_guard<std::mutex> lock(mLock);
+
+    char val[12+1];
+    snprintf(val, sizeof(val), "%u", rgbToBrightness(state));
+    write_file(lcd_backlight_filename, val);
+}
+
+} // namespace implementation
+}  // namespace V2_0
+}  // namespace light
+}  // namespace hardware
+}  // namespace android
