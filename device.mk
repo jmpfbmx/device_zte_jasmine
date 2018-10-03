53d779ce1ad64219cdba65970f1413eab0dbcb1b
Tom Marshall <tdm.code@gmail.com>
2018-10-03 09:35:55 -0700

    tulip: Use open source sensors multihal
    Change-Id: I3298bb5b0d6255220a448ed87faa1a4f276c77e0

--- device.mk
+++ device.mk
@@ -243,7 +243,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    sensors.tulip
 
 # Shims
 PRODUCT_PACKAGES += \
